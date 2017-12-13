7c05ba92d23cc454ff7e4854c76e4656d3848a1a
admin <admin@iMac3.local>
2017-12-13 17:34:08 +0100

    big up

---
+++ data/resources/views/accueil.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.base')
+
+@section('title')
+  Ultra Doooooooooooooooooooooooooom
+@endsection
+
+@section('main')
+  <div style="height: 800px!important; display: flex; align-items: center; justify-content: center;"> 
+    <p>Déchire tout pour une fois</p> </div>
+@endsection
